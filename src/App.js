597ca28fdf5c8a61076f36e96043a55279b29682
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-27 18:30:56 +0300

    INstall bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import NavBar from './components/Layout/NavBar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <>
+    <NavBar />
     
     </>
   );
