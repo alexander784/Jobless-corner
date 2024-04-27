00dadb94208a396981c6d0ee7b4f08dc68829412
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-27 19:02:30 +0300

    Create Homecomponent

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import './App.css';
 import NavBar from './components/Layout/NavBar';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Route,Routes
+} from 'react-router-dom';
+// import { Route, Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 
 function App() {
   return (
     <>
+    <Router>
+
     <NavBar />
+    <Routes>
+      <Route path='/' element={ <Home />} />
+
+      
+    </Routes>
+    </Router>
+
+  
+
     
     </>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='welcome-container'>
+        <h2>Crabs Jobs</h2>
+
+
+    </div>
+  )
+}
+
+export default Home;
